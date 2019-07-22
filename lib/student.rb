0b42652a12ff4a521d1b7d26732697d97e0d4cc0
Hillary Spitzer <hspitzer2@gmail.com>
2019-07-22 22:06:32 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,10 +1,13 @@
 class Student < User
 
   attr_writer :first_name, :last_name
+  attr_accessor :@knowledge
 
   def initialize
     @first_name = first_name
     @last_name = last_name
+    @knowledge = []
+
   end
 
 end
